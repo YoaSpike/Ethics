c0ec00bd9d2e887276db808147aa72b378deacc3
Dominic May <jack.thatch@gmail.com>
2015-03-31 12:38:57 +0800

    Cleanup

--- app/views/formdata/LoginForm.java
+++ app/views/formdata/LoginForm.java
@@ -9,13 +9,9 @@ public class LoginForm {
     @Constraints.Required
     @Constraints.Pattern(value="^[0-9]+[A-Za-z]?$", message="A valid Curtin ID is required")
     public String curtin_id;
-    // public void setCurtin_id(String val) { this.curtin_id = val; }
-    // public String getCurtin_id() {  return this.curtin_id; }
 
     @Constraints.Required
     public String encrypted_password;
-    // public void setEncrypted_password(String val) { this.encrypted_password = val; }
-    // public String getEncrypted_password() {  return this.encrypted_password; }
 
     // methods
     public String toString() {
