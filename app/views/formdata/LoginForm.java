14d6fa74418831916cf8f6bfa1261c5271a37c02
Dominic May <jack.thatch@gmail.com>
2015-03-31 15:27:17 +0800

    Restore accessors

--- app/views/formdata/LoginForm.java
+++ app/views/formdata/LoginForm.java
@@ -9,18 +9,17 @@ public class LoginForm {
     @Constraints.Required
     @Constraints.Pattern(value="^[0-9]+[A-Za-z]?$", message="A valid Curtin ID is required")
     public String curtin_id;
+    public void setCurtin_id(String val) { this.curtin_id = val; }
+    public String getCurtin_id() {  return this.curtin_id; }
 
     @Constraints.Required
     public String encrypted_password;
+    public void setEncrypted_password(String val) { this.encrypted_password = val; }
+    public String getEncrypted_password() {  return this.encrypted_password; }
 
     // methods
     public String toString() {
         return "<LoginForm \"" + curtin_id + "\" \"" + encrypted_password + "\">";
-    }
-
-    public String validate(Object thing) {
-        System.out.println("thing: " + thing);
-        return null;
     }
 
     public String validate() {
