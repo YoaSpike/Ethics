ebb02c7c386c51aef27b0278939dc8073aca262f
Dominic May <jack.thatch@gmail.com>
2015-05-05 14:57:13 +0800

    File uploads implemented
    Baring database interaction as of yet

--- app/controllers/EthicsApplication.java
+++ app/controllers/EthicsApplication.java
@@ -1,5 +1,14 @@
 package controllers;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.ArrayList;
+
+import com.google.common.io.ByteStreams;
+
 import play.*;
 import play.mvc.*;
 import play.data.Form;
@@ -26,5 +35,76 @@ public class EthicsApplication extends Controller {
 
     public static Result section2(Long id) {
         return ok("Section2 with id " + id);
+    }
+
+    public static Result section6(Long id) {
+        return ok(views.html.section6.render(
+            form(Section6Form.class)
+        ));
+    }
+
+    private static final Form<Section6Form> section6Form = form(Section6Form.class);
+
+    private static class ReworkedMultipart {
+        public Map<String,String[]> form_data;
+        public Map<String,Http.MultipartFormData.FilePart> files;
+    }
+    private static ReworkedMultipart remap_multipart(Http.MultipartFormData multipart) {
+        ReworkedMultipart res = new ReworkedMultipart();
+
+        res.form_data = new HashMap<String,String[]>(multipart.asFormUrlEncoded());
+        res.files = new HashMap<String,Http.MultipartFormData.FilePart>();
+
+        for (Http.MultipartFormData.FilePart file : multipart.getFiles()) {
+            res.form_data.put(file.getKey(), new String[] { file.getFilename() });
+            res.files    .put(file.getKey(), file);
+        }
+
+        return res;
+    }
+
+    public static Result section6_post(Long id) {
+        // if we want a file in a form, we need to do extra work
+
+        ReworkedMultipart multipart = remap_multipart(request().body().asMultipartFormData());
+        Form<Section6Form> filledForm = section6Form.bindFromRequest(multipart.form_data);
+
+        if(filledForm.hasErrors()) {
+            return badRequest(filledForm.errorsAsJson());
+
+        } else {
+            Section6Form data = filledForm.get();
+
+            saveAttachmentsToApplication(
+                data.attachments,
+                multipart.files,
+                id
+            );
+
+            return ok(data.toString());
+        }
+    }
+
+    public static void saveAttachmentsToApplication(List<Section6Form.Attachment> attachments, Map<String,Http.MultipartFormData.FilePart> files, Long id) {
+
+        play.Logger.info(attachments.size() + " attachments");
+        for (Section6Form.Attachment attach : attachments) {
+            String key = String.format("attachments[%d].file", attach.idx);
+            Http.MultipartFormData.FilePart fp = files.get(key);
+
+            byte[] bdata = null;
+            try {
+                bdata = ByteStreams.toByteArray(new FileInputStream(fp.getFile()));
+            } catch (IOException ioe) {}
+
+            play.Logger.info(String.format(
+                "Type: %s, version: %s, date: %s, file: %s %d bytes long",
+                attach.type,
+                attach.version,
+                attach.date,
+                attach.file,
+                bdata.length
+            ));
+        }
     }
 }
--- app/views/formdata/Section6Form.java
+++ app/views/formdata/Section6Form.java
@@ -1,13 +1,53 @@
 package views.formdata;
 
-import play.data.validation.*;
+import java.util.List;
+import javax.validation.*;
+
+import play.data.validation.Constraints.*;
+
+import org.joda.time.DateTime;
+
+
+enum AttachmentType {
+    PROTOCOL_RESEARCH_PROPOSAL,
+    RECRUITMENT_MATERIAL,
+    PARTICIPANT_INFORMATION_STATEMENT_AND_CONSENT_FORMS,
+    PARENT_INFORMATION_STATEMENT_AND_CONSENT_FORMS,
+    CHILD_INFORMATION_STATEMENT_AND_ASSENT_FORMS,
+    QUESTIONAIRES_SURVEY_INSTRUMENTS,
+    TRANSLATIONS_WHERE_LANGUAGES_OTHER_THAN_ENGLISH_ARE_USED,
+    RECRUITMENT_MATERIALS,
+    INVESTIGATOR_BROCHURE_OR_PRODUCT_INFORMATION,
+    WORKING_WITH_CHILDRENS_CARD,
+    SOL;
+}
+
 
 public class Section6Form {
-    // fields
-    public String potential_risk_or_harm;
+    @Valid
+    @Required
+    public List<Attachment> attachments;
+    public List<Attachment> getAttachments() { return this.attachments; }
+    public void setAttachments(List<Attachment> val) { this.attachments = val; }
 
-    public String risk_management_strategy;
+    public static class Attachment {
+        @Required
+        @Valid
+        public int type; // to be re-interpreted later
 
-    public boolean given_incentives;
-    public String incentives_details;
+        @Required
+        @Valid
+        public String version;
+
+        @Required
+        @Valid
+        public DateTime date;
+
+        @Required
+        @Valid
+        public String file; // filename only, we retrieve the file another way
+
+        @Valid
+        public int idx; // we need to know so we can match up the file data later
+    }
 }
