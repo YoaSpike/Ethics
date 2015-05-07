a0fcb3e9418d0540a84305c48bf24c5132f9f528
Dominic May <jack.thatch@gmail.com>
2015-05-07 23:22:23 +0800

    Remove remainders of demo system

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -40,22 +40,6 @@ public class Application extends Controller {
         return ok("Sent " + email);
     }
 
-    public static Result demo(String demo_name) {
-        if (demo_name.equals("section1")) {
-            return ok(views.html.demos.section1.render());
-
-        } else if (demo_name.length() > 7 && demo_name.substring(0, 7).equals("section")) {
-            String section_num = demo_name.substring(7);
-
-            return ok(views.html.demos.unimplemented.render(
-                Integer.parseInt(section_num)
-            ));
-
-        } else {
-            return badRequest("Bad in general");
-        }
-    }
-
     public static String get_section_link(Integer num) {
         return "/demos/" + (num == 0 ? "ethics_triage" : "section" + num);
     }
