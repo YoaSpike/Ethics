0776a59122f77cc500fb6aa95b96909735394cce
Dominic May <jack.thatch@gmail.com>
2015-05-07 19:12:47 +0800

    Parse attachment type automagically

--- app/views/formdata/Section6Form.java
+++ app/views/formdata/Section6Form.java
@@ -8,21 +8,6 @@ import play.data.validation.Constraints.*;
 import org.joda.time.DateTime;
 
 
-enum AttachmentType {
-    PROTOCOL_RESEARCH_PROPOSAL,
-    RECRUITMENT_MATERIAL,
-    PARTICIPANT_INFORMATION_STATEMENT_AND_CONSENT_FORMS,
-    PARENT_INFORMATION_STATEMENT_AND_CONSENT_FORMS,
-    CHILD_INFORMATION_STATEMENT_AND_ASSENT_FORMS,
-    QUESTIONAIRES_SURVEY_INSTRUMENTS,
-    TRANSLATIONS_WHERE_LANGUAGES_OTHER_THAN_ENGLISH_ARE_USED,
-    RECRUITMENT_MATERIALS,
-    INVESTIGATOR_BROCHURE_OR_PRODUCT_INFORMATION,
-    WORKING_WITH_CHILDRENS_CARD,
-    SOL;
-}
-
-
 public class Section6Form {
     @Valid
     @Required
@@ -33,7 +18,7 @@ public class Section6Form {
     public static class Attachment {
         @Required
         @Valid
-        public int type; // to be re-interpreted later
+        public AttachmentType type;
 
         @Required
         @Valid
@@ -42,7 +27,6 @@ public class Section6Form {
         @Required
         @Valid
         public DateTime date;
-
 
         @Valid
         public int idx; // we need to know so we can match up the file data later
