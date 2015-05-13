24442a05a3abf3b4163045f439a0ba5c2757c42f
Dominic May <jack.thatch@gmail.com>
2015-05-13 19:09:41 +0800

    Validation explanatory comment

--- app/business/Secured.java
+++ app/business/Secured.java
@@ -7,6 +7,8 @@ import play.mvc.Http.*;
 public class Secured extends Security.Authenticator {
     @Override
     public String getUsername(Context ctx) {
+        // we don't need to manually validate the session, in theory,
+        // because play signs and validates session variables
         return ctx.session().get("email");
     }
 
