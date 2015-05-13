3abc04c4460ac5470c06e86f4a15f43259f26f60
Dominic May <jack.thatch@gmail.com>
2015-05-13 12:51:52 +0800

    Start sketching out form for section 7

--- app/controllers/application/Section7.java
+++ app/controllers/application/Section7.java
@@ -8,12 +8,19 @@ import views.formdata.*;
 
 
 public class Section7 extends Controller {
+    private static final Form<Section7Form> section7Form = form(Section7Form.class);
+
     public static Result get(Long id) {
         int section_num = 7;
         return ok(views.html.application.section7.render(id, section_num));
     }
 
     public static Result post(long id) {
-        return ok("Hello!");
+        Form<Section7Form> filledForm = section7Form.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return badRequest(filledForm.errorsAsJson());
+        } else {
+            return ok(filledForm.data().toString());
+        }
     }
 }
---
+++ app/views/formdata/Section7Form.java
@@ -0,0 +1,29 @@
+package views.formdata;
+
+import java.util.List;
+import javax.validation.*;
+
+import play.data.validation.Constraints.*;
+
+import org.joda.time.DateTime;
+
+
+public class Section7Form {
+    @Valid
+    String pi_name;
+
+    @Valid
+    DateTime pi_sig_date;
+
+    @Valid
+    String pi_sig;
+
+    @Valid
+    String hos_name;
+
+    @Valid
+    DateTime hos_sig_date;
+
+    @Valid
+    String hos_sig;
+}
