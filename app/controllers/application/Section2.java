1d6b5d771497da00939d46a238402533cd4729a8
Dominic May <jack.thatch@gmail.com>
2015-05-14 11:01:01 +0800

    Work on section 2 form

--- app/controllers/application/Section2.java
+++ app/controllers/application/Section2.java
@@ -4,16 +4,24 @@ import play.mvc.*;
 import play.data.Form;
 import static play.data.Form.*;
 
-import views.formdata.*;
+import views.formdata.application.*;
 
 
 public class Section2 extends Controller {
+    private static final Form<Section2Form> section2Form = form(Section2Form.class);
+
     public static Result get(Long id) {
         int section_num = 2;
         return ok(views.html.application.section2.render(id, section_num));
     }
 
     public static Result post(long id) {
-        return ok("Hello!");
+        Form<Section2Form> filledForm = section2Form.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return ok(filledForm.errorsAsJson());
+
+        } else {
+            return ok(filledForm.get().toString());
+        }
     }
 }
--- app/views/formdata/application/Section2Form.java
+++ app/views/formdata/application/Section2Form.java
@@ -1,13 +1,18 @@
 package views.formdata.application;
 
+import javax.validation.*;
 import play.data.validation.*;
 
 public class Section2Form {
     // fields
+    @Valid
     public String potential_risk_or_harm;
 
+    @Valid
     public String risk_management_strategy;
 
+    @Valid
     public boolean given_incentives;
+    @Valid
     public String incentives_details;
 }
