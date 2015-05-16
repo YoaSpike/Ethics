5d5d2a38a9a704fcefae7c04f072177bf53bcddb
Dominic May <jack.thatch@gmail.com>
2015-05-16 23:05:27 +0800

    Hook up section 1 form flow
    Still needs work; the enums, ought to be probably moved to the business
    package, that they might be used in the models.

--- app/controllers/application/Section1.java
+++ app/controllers/application/Section1.java
@@ -4,16 +4,23 @@ import play.mvc.*;
 import play.data.Form;
 import static play.data.Form.*;
 
-import views.formdata.*;
+import views.formdata.application.*;
 
 
 public class Section1 extends Controller {
+    private static final Form<Section1Form> section1Form = form(Section1Form.class);
+
     public static Result get(Long id) {
         int section_num = 1;
         return ok(views.html.application.section1.render(id, section_num));
     }
 
     public static Result post(long id) {
-        return ok("Hello!");
+        Form<Section1Form> filledForm = section1Form.bindFromRequest();
+        if(filledForm.hasErrors()) {
+            return badRequest(filledForm.errorsAsJson());
+        } else {
+            return ok("Hello!" + filledForm.get());
+        }
     }
 }
---
+++ app/views/formdata/application/Section1Form.java
@@ -0,0 +1,66 @@
+package views.formdata.application;
+
+import java.util.List;
+
+import javax.validation.*;
+import play.data.validation.Constraints.*;
+
+enum ProjectType {
+    STAFF_PROJECT,
+    UNDERGRADUATE,
+    HONOURS,
+    MASTERS_BY_COURSEWORK,
+    MASTERS_BY_RESEARCH,
+    PHD,
+    OTHER;
+};
+enum Role {CI, SUPERVISOR, STUDENT;};
+
+
+public class Section1Form {
+    @Valid
+    public String project_title;
+
+    @Valid
+    public ProjectType project_type;
+
+    @Valid
+    public String background;
+
+    @Valid
+    public String aims_and_hypothesis;
+
+    @Valid
+    public String methods;
+
+    @Valid
+    public Person pi;
+
+    @Valid
+    public List<Person> coinvestigators;
+
+    @Valid
+    public Person contact_person;
+
+    public static class Person {
+        // not all of these are entered for every single use
+        @Valid
+        public String name;
+        @Valid
+        public String area;
+        @Valid
+        public String id;
+        @Valid
+        public Role role;
+        @Valid
+        public Boolean candidacy_approved;
+        @Valid
+        public Boolean research_integrity; // aka SOL
+
+        @Valid
+        public String telephone;
+        @Valid
+        @Email
+        public String email;
+    }
+}
