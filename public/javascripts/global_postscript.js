169bee843301671b4aabd4c8cfcf4a1c9ea56b98
Spike Mak <msh.spike@gmail.com>
2015-05-19 00:44:37 +0800

    Make use of global_postscript.js; ongoing email HTML design; also try using SCSS :P

--- public/javascripts/global_postscript.js
+++ public/javascripts/global_postscript.js
@@ -1,1 +1,24 @@
-$("#nav-sidebar .panel-body").css("max-height", $(window).height()-150);
+$(document).ready(function() {
+    $(".compulsory").focusout(function() {
+        // Each input contains attribute: data-qid="#"
+        var qid = $(this).data("qid");
+        
+        // Append "has-error" class to the input if no value has been entered.
+        // This will only be apply to text field or text area, as other inputs always contain a value.
+        if ($(this).val() != "") {
+            $(this).closest("div").removeClass('has-error');
+            $(this).next(".error-msg").remove();
+        } else {
+            $(this).closest("div").addClass('has-error');
+            if ($(this).next(".error-msg").length == 0)
+                $('<div class="error-msg">Please fill in the blank.</div>').insertAfter($(this));
+        }
+
+        // Toggle "incomplete" class to the question's link on sidebar.
+        if ($(this).val() != "") {
+            $("#"+qid+"Link").removeClass("incomplete");
+        } else {
+            $("#"+qid+"Link").addClass("incomplete");
+        }
+    });
+});
