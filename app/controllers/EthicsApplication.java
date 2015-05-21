63bfe40f980832385f951bec5d19ef5a488e9d47
Yoakim <yoakimpersson@hotmail.com>
2015-05-21 12:27:48 +0800

    Be able to create applications! - Dom

--- app/controllers/EthicsApplication.java
+++ app/controllers/EthicsApplication.java
@@ -24,4 +24,30 @@ public class EthicsApplication extends Controller {
     public static Result application_summary(Long id) {
         return ok("Summararily");
     }
+
+    public static Result new_get() {
+    	UserModel user = controllers.Accounts.getCurrentUser();
+
+    	ApplicationModel app = new ApplicationModel();
+    	app.userModel = user;
+
+    	// app.section0 = new Section0Model();
+    	app.section1 = new Section1Model();
+    	app.section2 = new Section2Model();
+    	app.section3 = new Section3Model();
+    	app.section4 = new Section4Model();
+    	// app.section5 = new Section5Model();
+
+    	app.section1.save();
+    	app.section2.save();
+    	app.section3.save();
+    	app.section4.save();
+
+    	app.save();
+    	app.refresh(); // get the id back from the db
+    	user.applications.add(app);
+    	user.save();
+
+    	return redirect(routes.EthicsApplication.application_summary(app.id));
+    }
 }
