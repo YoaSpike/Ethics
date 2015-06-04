91ffd55ec7064fa7d48cfb8c5f3555cefd22ef72
Dominic May <jack.thatch@gmail.com>
2015-06-04 12:20:40 +0800

    More compatible

--- add_user.py
+++ add_user.py
@@ -1,4 +1,33 @@
+from __future__ import print_function
+
 import requests
+
+try:
+    input = raw_input
+except NameError:
+    pass
+
+try:
+    from urllib import urlopen, urlencode
+except ImportError:
+    from urllib.parse import urlencode
+    from urllib.request import urlopen
+
+
+class Req:
+    class Res:
+        def __init__(self, text):
+            self.text = text.decode()
+
+    def post(self, url, data):
+        return self.Res(urlopen(url + '?' + urlencode(data)).read())
+
+
+try:
+    import requests
+except ImportError:
+    requests = Req()
+
 
 q = input('Online? [Y/N] ')
 BASE = (
